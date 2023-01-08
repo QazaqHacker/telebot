722126f1780d73dd41e8db18e61f48443662f166
Isko <hackerqazaq@gmail.com>
2023-01-08 06:08:18 +0500

    hools

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
-import {useTelegram} from "./hooks/useTelegram";
+import {useTelegram} from "./hools/useTelegram";
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
